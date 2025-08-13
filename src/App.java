61b1850b1f7fe222c0b0a473f1e4040d0586c7ee
jorgeStevenR <rodrigesj500@gmail.com>
2025-08-13 18:20:14 -0500

    First commit

---
+++ src/App.java
@@ -0,0 +1,12 @@
+import javax.swing.JOptionPane;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        double precioOriginal =Double.parseDouble(JOptionPane.showInputDialog("Digite el precio original del producto"));
+
+        double descuento = Double.parseDouble(JOptionPane.showInputDialog("Digite el descuento del producto"));
+
+
+
+    }
+}
